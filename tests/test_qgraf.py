f0135d84a42f8d6d936a18a02f5d0690a66c3958
Alexander Puck Neuwirth <APN-Pucky@users.noreply.github.com>
2023-02-06 09:27:30 +0100

    Update test_qgraf.py

--- tests/test_qgraf.py
+++ tests/test_qgraf.py
@@ -7,8 +7,8 @@ from feynmodel.interface.ufo import load_ufo_model
 def test_load_qgraf():
     """Test loading a qgraf model"""
     # Read file as string
-    dir = pathlib.Path(__file__).parent.resolve()
-    with open(dir / ".." / "models" / "qgraf" / "test.qgraf", "r") as f:
+    d = pathlib.Path(__file__).parent.resolve()
+    with open(d / ".." / "models" / "qgraf" / "test.qgraf", "r") as f:
         s = f.read()
         fm = qgraf_to_feynmodel(s)
         print(fm.particles)
@@ -17,15 +17,15 @@ def test_load_qgraf():
 
 def test_load_ufo():
     """Test loading a UFO model"""
-    dir = pathlib.Path(__file__).parent.resolve()
-    fm = load_ufo_model(dir / ".." / "models" / "ufo" / "sm")
+    d = pathlib.Path(__file__).parent.resolve()
+    fm = load_ufo_model(d / ".." / "models" / "ufo" / "sm")
     print(feynmodel_to_qgraf(fm, True))
 
 
 def test_idempotency():
     """Test that qgraf_to_feynmodel(feynmodel_to_qgraf(fm)) == fm"""
-    dir = pathlib.Path(__file__).parent.resolve()
-    fm = load_ufo_model(dir / ".." / "models" / "ufo" / "sm")
+    d = pathlib.Path(__file__).parent.resolve()
+    fm = load_ufo_model(d / ".." / "models" / "ufo" / "sm")
     fm2 = qgraf_to_feynmodel(feynmodel_to_qgraf(fm, True))
     # assert len(fm.particles) == len(fm2.particles), "Number of particles does not match"
     # assert len(fm.vertices) == len(fm2.vertices), "Number of vertices does not match"
